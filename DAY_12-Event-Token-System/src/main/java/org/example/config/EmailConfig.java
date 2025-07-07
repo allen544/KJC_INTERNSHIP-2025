cd09d19a94e51b5edf4479c702b647cced5ef6bb
Allen Saji Thomas <allensajithomas@gmail.com>
2025-07-07 14:57:34 +0530

    Update EmailConfig.java

--- DAY_12-Event-Token-System/src/main/java/org/example/config/EmailConfig.java
+++ DAY_12-Event-Token-System/src/main/java/org/example/config/EmailConfig.java
@@ -3,7 +3,7 @@ package org.example.config;
 public class EmailConfig {
     public static final String SMTP_HOST = "smtp.gmail.com";
     public static final String SMTP_PORT = "587";
-    public static final String USERNAME = "allenprowork@gmail.com";
-    public static final String PASSWORD = "udgt xvhp mijg wmrf";  // App Password (no spaces)
-    public static final String FROM = "allenprowork@gmail.com";
+    public static final String USERNAME = "Your Gmail";
+    public static final String PASSWORD = "Your Google App Password";  // App Password (no spaces)
+    public static final String FROM = "Your Gmail";
 }
