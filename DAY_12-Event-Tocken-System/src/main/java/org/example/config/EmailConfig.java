3a42b3b589905f81cb13b1662a5932a80474cd9a
Allen Saji Thomas <allensajithomas@gmail.com>
2025-07-07 12:28:16 +0530

    Delete DAY_12-Event-Tocken-System directory

--- DAY_12-Event-Tocken-System/src/main/java/org/example/config/EmailConfig.java
+++
@@ -1,9 +0,0 @@
-package org.example.config;
-
-public class EmailConfig {
-    public static final String SMTP_HOST = "smtp.gmail.com";
-    public static final String SMTP_PORT = "587";
-    public static final String USERNAME = "allenprowork@gmail.com";
-    public static final String PASSWORD = "udgt xvhp mijg wmrf";  // App Password (no spaces)
-    public static final String FROM = "allenprowork@gmail.com";
-}
